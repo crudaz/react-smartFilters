8adfd88eecef78f667f54cfa36f27812f9a1ad93
crudaz <romerocruz@gmail.com>
2019-07-10 22:46:36 -0500

    feat: icons and logo with material UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.png';
 
-//Components
+
+// Components
 import SmartFilter from './components/smartFilter';
 import MenuTop from './components/menuTop';
 import Search from './components/search'
 import DeliveryDetail from './components/deliveryDetail';
 import DeliveryStatus from './components/deliveryStatus';
 
+// Icons
+import sale from './assets/icons/sale.png';
+import calendar from './assets/icons/calendar.png';
+import heart from './assets/icons/heart.png';
+
+
 function App() {
+
+  const filters = [
+    { icon: sale, op: 'disabled' },
+    { icon: calendar, op: 'disabled' },
+    { icon: heart, op: 'disabled' }
+  ];
+
   return (
     <div className="app">      
       <div>
-        <SmartFilter />
+        <img className="logo" src={logo} alt="" />
+        
+        <p>Smart Filters</p>
+        <hr />
+        <SmartFilter list={filters}/>
+        <hr />
+        
         <DeliveryStatus />
       </div>
 
