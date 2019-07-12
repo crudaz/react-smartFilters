72d9121ba779571be14e1f9219b838dbfa52fccc
crudaz <romerocruz@gmail.com>
2019-07-11 23:38:46 -0500

    style: add style to component

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
 import TextField from '@material-ui/core/TextField';
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        backgroundColor: 'white'
+    },
+  }));
+
 const SearchBar = () => {
+    const classes = useStyles();
     return (
-        <TextField
+        <TextField className={classes.root}
             id="search"
             label="search"
             placeholder="Search"
