036b191eec8c535e163bee8bc55cbcff0abf29d0
crudaz <romerocruz@gmail.com>
2019-07-14 22:30:00 -0500

    feat: add icons

--- src/App.js
+++ src/App.js
@@ -16,22 +16,30 @@ import sale from './assets/icons/sale.png';
 import calendar from './assets/icons/calendar.png';
 import heart from './assets/icons/heart.png';
 
+import HomeIcon from '@material-ui/icons/Home';
+import MailIcon from '@material-ui/icons/Mail';
+import StarIcon from '@material-ui/icons/Star';
+import SettingsIcon from '@material-ui/icons/Settings';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+import AddCircle from '@material-ui/icons/AddCircle';
+
 import Checkbox from '@material-ui/core/Checkbox';
 
 function App() {
 
   const menuList = [
-    { id: 1, name: 'Home', icon: 'calendar' },
-    { id: 2, name: 'Messages', icon: 'calendar' },
-    { id: 3, name: 'Wishlist', icon: 'calendar' },
-    { id: 4, name: 'Settings', icon: 'calendar' },
-    { id: 5, name: 'My Account', icon: 'calendar' }
+    { id: 1, name: 'Home', icon: <HomeIcon /> },
+    { id: 2, name: 'Messages', icon: <MailIcon /> },
+    { id: 3, name: 'Wishlist', icon: <StarIcon /> },
+    { id: 4, name: 'Settings', icon: <SettingsIcon /> },
+    { id: 5, name: 'My Account', icon: <AccountCircleIcon /> }
   ];
   
   const filters = [
-    { id: 1, icon: sale, op: 'disabled' },
-    { id: 2, icon: calendar, op: 'disabled' },
-    { id: 3, icon: heart, op: 'disabled' }
+    { id: 1, icon: sale, checked: true },
+    { id: 2, icon: calendar, checked: true },
+    { id: 3, icon: heart, checked: true }
   ];
   
   const deliveryList = [
@@ -40,7 +48,7 @@ function App() {
       date: 'Monday 10th 2:28 PM', 
       from: 'Houston, TX, 33619', 
       to: 'Atlanta, GA, 30123',
-      price: '250',
+      price: '250.00',
       quantity: 1
     },
     {
@@ -48,7 +56,7 @@ function App() {
       date: 'Monday 10th 2:28 PM', 
       from: 'Houston, TX, 33619', 
       to: 'Atlanta, GA, 30123',
-      price: '250',
+      price: '250.00',
       quantity: 1
     },
     {
@@ -56,7 +64,7 @@ function App() {
       date: 'Monday 10th 2:28 PM', 
       from: 'Houston, TX, 33619', 
       to: 'Atlanta, GA, 30123',
-      price: '250',
+      price: '250.00',
       quantity: 1
     },
   ];
@@ -70,6 +78,12 @@ function App() {
   const handleChange = name => event => {
     setState({ ...state, [name]: event.target.checked });
   };
+
+  function AddIcon(props) {
+    return (
+        <AddCircle {...props} />
+    );
+}
   
   return (
     <div className="app">      
@@ -113,8 +127,7 @@ function App() {
           <DeliveryDetail list={deliveryList} />
         </div>
 
-        <i className="material-icons">add_circle</i>
-
+        <AddIcon className="addIcon" />
       </div>
 
     </div>
